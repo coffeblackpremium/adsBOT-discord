92812e0c4602da3f166f8aef510b685fc09f9fcf
João Damaceno <joaodamaceno188@gmail.com>
2022-03-18 16:21:19 -0300

    Bot.py Initialized

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+import datetime
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>', description="BOT da ADS Fasipe")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Salve Salve", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    await ctx.send(embed=embed)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Streaming(name="Como Centralizar uma DIV", url="www.youtube.com/fabioakita"))
+    print('Estou Pronto!')
+bot.run('NzY1Nzc2NTE5Njc0OTg2NTQ2.X4Zu9Q.avAlQXcjPR8_jURuNEjVgksRofU')
