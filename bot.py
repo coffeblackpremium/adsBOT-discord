f783304c0bc017b453d646d44422d4f54bcd122f
João Damaceno <joaodamaceno188@gmail.com>
2022-03-18 23:04:22 -0300

    Command Play add

--- bot.py
+++ bot.py
@@ -14,16 +14,24 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def info(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="Salve Salve", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+async def creditos(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Salve familia. Este BOT é pertencente a Atlética Virais", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
     await ctx.send(embed=embed)
+
+@bot.command()
+async def virais(ctx):
+    await ctx.send(f'Essa atlética é do balaco baco  😎')
 
 @bot.command()
 async def clear(ctx, limit:int):
     await ctx.channel.purge(limit=limit+1)
     await ctx.send(f'O {ctx.author.mention} Apagou {limit} Mensagens.')
 
-
+@bot.command()
+async def play(ctx, url:str):
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Sala de estudo 1')
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    await voiceChannel.connect()
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name="Como Centralizar uma DIV", url="www.youtube.com/fabioakita"))
