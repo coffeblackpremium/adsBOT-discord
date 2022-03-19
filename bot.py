2e8f9849f1c32c6339e90647920be13b015b5b1a
João Damaceno <joaodamaceno188@gmail.com>
2022-03-19 00:04:42 -0300

    Play and Leave add

--- bot.py
+++ bot.py
@@ -29,9 +29,24 @@ async def clear(ctx, limit:int):
 
 @bot.command()
 async def play(ctx, url:str):
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Sala de estudo 1')
+    channel = ctx.message.author.voice.channel
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name=channel.name)
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    await voiceChannel.connect()
+    
+    if voice == None:
+        await voiceChannel.connect()
+    else:
+        await voice.move_to(channel)
+@bot.command()
+async def leave(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice is None:
+        if voice.is_connected():
+            await voice.disconnect()
+        else:
+            await ctx.send(f'{ctx.author.mention} O BOT não está conectado em nenhuma sala!')
+    else:
+        await ctx.send(f'{ctx.author.mention} O BOT não está conectado em nenhuma sala!')
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name="Como Centralizar uma DIV", url="www.youtube.com/fabioakita"))
